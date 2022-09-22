f9adb33f01aa7e2614448dfcd0ec31d7325aeccf
Tommy Graves <tommy@rwx.com>
2022-09-22 14:08:52 -0400

    Properly handle the case where job-matrix is not set

--- src/utils.ts
+++ src/utils.ts
@@ -48,11 +48,12 @@ function parseIfFilesNotFound(input: string): IfFilesNotFound {
 }
 
 export function getInputs(): Inputs {
+  const matrix = core.getInput('job-matrix')
   return {
     accountName: github.context.repo.owner,
     artifacts: JSON.parse(core.getInput('artifacts')) as InputArtifact[],
     ifFilesNotFound: parseIfFilesNotFound(core.getInput('if-files-not-found')),
-    jobMatrix: JSON.parse(core.getInput('job-matrix')),
+    jobMatrix: matrix ? JSON.parse(matrix) : null,
     jobName: core.getInput('job-name') || github.context.job,
     repositoryName: github.context.repo.repo,
     runId: github.context.runId.toString(),

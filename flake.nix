a99f6811a6027e77b6ce1957e61da4f37c38c0eb
Michael Glass <me@mike.is>
2022-10-19 12:04:34 +0200

    properly set formatter and use a formatter that supporst directory formatting (#127)

--- flake.nix
+++ flake.nix
@@ -7,23 +7,17 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { system = system; };
-      mayNeedRosetta = pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64;
-      rosettaArch = "x86_64-darwin";
-    in
-    {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          nixfmt # formatting _this file!_
-          nodejs # locked to 16.16.0
-        ];
-      };
-    });
+      let pkgs = import nixpkgs { system = system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              nodejs # locked to 16.16.0
+            ];
+        };
+      });
 }
